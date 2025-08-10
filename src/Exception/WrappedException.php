c21de840686e1801a49744c0583b7610a49839f3
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2025-08-10 17:34:40 +0500

    Add `StorageProviderInterface` to define storage provider contracts and update docblocks in `WrappedException`

--- src/Exception/WrappedException.php
+++ src/Exception/WrappedException.php
@@ -1,20 +1,23 @@
 <?php
-declare(strict_types=1);
-
-/**
+/*
  * Part of the "charcoal-dev/base" package.
  * @link https://github.com/charcoal-dev/base
  */
+
+declare(strict_types=1);
 
 namespace Charcoal\Base\Exception;
 
 /**
  * A custom exception class that wraps another exception as its previous throwable.
- * This class allows for enhanced exception handling by providing the ability to wrap and rethrow
- * an existing throwable with an optional custom message and code.
  */
 class WrappedException extends \Exception
 {
+    /**
+     * @param \Throwable $previous
+     * @param string|null $message
+     * @param int $code
+     */
     public function __construct(\Throwable $previous, ?string $message = null, int $code = 0)
     {
         parent::__construct($message ?? static::class, $code, $previous);
