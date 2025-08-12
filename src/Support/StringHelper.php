3bb2ded21d3fdd1a886f7d7820f7b436e67d440b
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2025-08-12 19:21:38 +0500

    Add `StringHelper` with utility methods for space cleaning and trimmed value handling

---
+++ src/Support/StringHelper.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Part of the "charcoal-dev/base" package.
+ * @link https://github.com/charcoal-dev/base
+ */
+
+declare(strict_types=1);
+
+namespace Charcoal\Base\Support;
+
+/**
+ * A helper class that provides utility methods for working with strings.
+ */
+class StringHelper
+{
+    /**
+     * @param string $input
+     * @return string
+     */
+    public static function cleanSpaces(string $input): string
+    {
+        return trim(preg_replace('/(\s+)/', ' ', $input));
+    }
+
+    /**
+     * @param mixed $input
+     * @return string|null
+     */
+    public static function getTrimmedOrNull(mixed $input): ?string
+    {
+        if (is_string($input)) {
+            $input = trim($input);
+            if (!$input) {
+                return null;
+            }
+
+            return $input;
+        }
+
+        return null;
+    }
+}
