ebe8237806a6caf6dd8789b1e7ad4b0c21f65f5b
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2025-08-13 19:30:07 +0500

    Add `CheckedKeyValue` and `BatchEnvelope` classes to support encapsulated key-value handling and batch processing with exception strategies.

---
+++ src/Support/Data/BatchEnvelope.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Part of the "charcoal-dev/base" package.
+ * @link https://github.com/charcoal-dev/base
+ */
+
+declare(strict_types=1);
+
+namespace Charcoal\Base\Support\Data;
+
+use Charcoal\Base\Enums\ExceptionAction;
+
+/**
+ * This class provides a mechanism for processing a batch of data with specified
+ * behavior in case of exceptions. It also optionally allows for logging errors
+ * through a provided closure.
+ *
+ * Use ExceptionAction to define how exceptions will be handled during execution.
+ * If ExceptionAction::Log is chosen, an error logger closure must be provided.
+ * @see ExceptionAction
+ */
+readonly class BatchEnvelope
+{
+    /**
+     * @param array $data
+     * @param ExceptionAction $onError
+     * @param (\Closure(string|int, \Throwable): void)|null $errorLogger
+     */
+    public function __construct(
+        public array           $data,
+        public ExceptionAction $onError = ExceptionAction::Throw,
+        public ?\Closure       $errorLogger = null,
+    )
+    {
+        if ($this->onError === ExceptionAction::Log && $this->errorLogger === null) {
+            throw new \InvalidArgumentException("Error logger must be provided when logging errors");
+        }
+    }
+}
