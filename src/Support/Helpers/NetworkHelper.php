a81e1c0d8b3b2441cde26d97ab26ee388974e8ac
Furqan Siddiqui <hello@furqansiddiqui.com>
2025-08-25 03:01:20 +0500

    Refactor NetworkHelper to enhance CIDR matching logic with dedicated binary parsing methods and improve readability

--- src/Support/Helpers/NetworkHelper.php
+++ src/Support/Helpers/NetworkHelper.php
@@ -30,30 +30,90 @@ abstract readonly class NetworkHelper
     /**
      * Matches a given IP address against a list of CIDR notations to determine
      * if the IP falls within any of the specified ranges.
+     * IP address in non-binary format is converted to binary format before matching.
+     * CIDR notations are parsed into binary format before matching.
      */
-    public static function ipInCidr(string $ip, array $cidrList): bool
+    final public static function ipInCidr(string $ip, array $cidrList): bool
     {
         $ipBin = @inet_pton($ip);
         if (!$ipBin) {
             return false;
         }
 
-        $len = strlen($ipBin);
         foreach ($cidrList as $cidr) {
-            [$net, $prefix] = array_pad(explode("/", $cidr, 2), 2, null);
-            $netBin = @inet_pton($net ?? "");
-            $pfx = is_numeric($prefix) ? (int)$prefix : -1;
-            if ($netBin === false || strlen($netBin) !== $len || $pfx < 0 || $pfx > ($len * 8)) {
+            $cidrBinary = self::parseCidrToBinary($cidr);
+            if (!$cidrBinary) {
                 continue;
             }
 
-            $mask = self::mask($len, $pfx);
-            if ((($ipBin & $mask) === ($netBin & $mask))) {
+            if (self::ipInCidrBinary($ipBin, true, $cidrBinary[0], $cidrBinary[1])) {
                 return true;
             }
         }
 
         return false;
+    }
+
+    /**
+     * Checks if the given IP address falls within the specified CIDR range.
+     * IP address in non-binary format is converted to binary format before matching.
+     * CIDR range is parsed into binary format before matching.
+     */
+    final public static function ipInCidrBinary(
+        string $ip,
+        bool   $ipIsBinary,
+        string $cidrNetwork,
+        string $cidrMask,
+    ): bool
+    {
+        if (!$cidrNetwork || !$cidrMask) {
+            return false;
+        }
+
+        $ipBin = $ipIsBinary ? $ip : @inet_pton($ip);
+        if (!$ipBin) {
+            return false;
+        }
+
+        return strlen($ipBin) === strlen($cidrMask) &&
+            ($ipBin & $cidrMask) === ($cidrNetwork & $cidrMask);
+    }
+
+    /**
+     * Parses a CIDR notation into binary format.
+     * @return array{string,string}|false
+     */
+    final public static function parseCidrToBinary(string $cidr): array|false
+    {
+        [$net, $prefix] = array_pad(explode("/", $cidr, 2), 2, null);
+        $netBin = @inet_pton($net ?? "");
+        if ($netBin === false) {
+            return false;
+        }
+
+        $bits = strlen($netBin) * 8;
+        $pfx = is_numeric($prefix) ? max(0, min((int)$prefix, $bits)) : $bits;
+        $mask = self::mask(strlen($netBin), $pfx);
+        $network = $netBin & $mask;
+        return [$network, $mask];
+    }
+
+    /**
+     * Parses a list of CIDR notations into binary format.
+     * @param array $cidrList
+     * @return array<string,array{string,string}>
+     */
+    final public static function parseCidrListToBinary(array $cidrList): array
+    {
+        $parsed = [];
+        foreach ($cidrList as $cidr) {
+            $cidrBinary = self::parseCidrToBinary($cidr);
+            if ($cidrBinary) {
+                $parsed[$cidr] = $cidrBinary;
+            }
+        }
+
+        return $parsed;
     }
 
     /**
