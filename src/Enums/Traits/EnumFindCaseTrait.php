54dbba15e5719f6b73ece06bcc0eef5997140969
Furqan Siddiqui <hello@furqansiddiqui.com>
2025-11-19 13:08:27 +0400

    Add case-insensitive support to `EnumFindCaseTrait` via `find` method

--- src/Enums/Traits/EnumFindCaseTrait.php
+++ src/Enums/Traits/EnumFindCaseTrait.php
@@ -15,10 +15,18 @@ trait EnumFindCaseTrait
 {
     use EnumMappingTrait;
 
-    public static function find(string $case): ?static
+    public static function find(string $case, bool $caseSensitive = true): ?static
     {
+        if (!$caseSensitive) {
+            $case = strtolower($case);
+        }
+
         foreach (static::cases() as $c) {
             $c2 = static::isBackedEnum() ? $c->value : $c->name;
+            if (!$caseSensitive) {
+                $c2 = strtolower($c2);
+            }
+
             if ($c2 === $case) {
                 return $c;
             }
